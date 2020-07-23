5b945e7bfab4498404bba6ed5f21b14874fae986
Ranjith Ramachandra <ranjith19@gmail.com>
2020-07-23 14:05:58 +0530

    wip

--- merge.py
+++ merge.py
@@ -95,7 +95,16 @@ for i, f in enumerate(files):
             full_spec['components']['schemas'].update(spec['components']['schemas'])
 
 full_spec['info']['title'] = 'LSP Lending spec'
-full_spec['info']['description'] = open("README.md").read()
+full_spec['info']['description'] = """
+
+Sourcing, identity verification, underwriting, disbursement, recollections and dispute management are few of the many responsibilities managed by a lender today.  Historically, the best lenders have had to excel at each of these skills in order to maximise their profit pools and serve the larger and larger markets.
+
+LSP is an initiative to unbundle lending and enable the creation of specialized entities, each specialized at one part of the job. Therefore, we envision the future of lending to be a partnership between multiple firms individually focused on sourcing/distribution, identity verification, underwriting, capital arrangement, recollections, etc.
+
+This LSP is being created to showcase the power of the above idea. In its simplest form, it's a loan marketplace that enables SMEs to compare loan offers from multiple lenders and choose the best one. In its more advanced version, This LSP would be akin to a borrower’s financial advisor, looking after their interests, fetching the best offers and advising the customer to make good decisions.
+
+In the longer run, it is envisioned that many more This LSP-like applications (LSPs) will be created. Each of these would focus on distinct borrower pools and build the specialized experiences suited to their customers. This would allow lenders to focus purely on their underwriting and collections logic and cater to diverse collaborations with the LSPs.
+"""
 
 
 
